cd7b2b796d3fd60bb391bcaa43b37964996a955b
subrata biswas <subratabiswasofficial@gmail.com>
2022-05-17 15:32:42 +0530

    adding esp8266 api call

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,67 @@
 #include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecureBearSSL.h>
+/* */
+#include <ArduinoJson.h>
 
 ESP8266WiFiMulti WiFiMulti;
-
 void setup()
 {
-  Serial.begin(9600);
-  // Serial.setDebugOutput(true);
-
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
+  Serial.begin(115200);
   for (uint8_t t = 4; t > 0; t--)
   {
     Serial.printf("[SETUP] WAIT %d...\n", t);
     Serial.flush();
     delay(1000);
   }
-
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP("DIGISOL", "12345699");
 }
 
 void loop()
 {
-  // wait for WiFi connection
   if ((WiFiMulti.run() == WL_CONNECTED))
   {
+    WiFiClient client;
+    HTTPClient http;
 
-    std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+    http.begin(client, "http://jsonplaceholder.typicode.com/todos/1");
+    // http.addHeader("Content-Type", "application/json");
 
-    // Or, if you happy to ignore the SSL certificate, then use the following line instead:
-    client->setInsecure();
+    // int httpCode = http.POST("{\"name\":\"subrata biswas\",\"age\":12}");
+    int httpCode = http.GET();
 
-    HTTPClient https;
-
-    Serial.print("[HTTPS] begin...\n");
-    if (https.begin(*client, "https://jsonplaceholder.typicode.com/todos/1"))
-    { // HTTPS
-
-      Serial.print("[HTTPS] GET...\n");
-      // start connection and send HTTP header
-      int httpCode = https.GET();
-
-      // httpCode will be negative on error
-      if (httpCode > 0)
+    if (httpCode > 0)
+    {
+      if (httpCode == HTTP_CODE_OK)
       {
-        // HTTP header has been send and Server response header has been handled
-        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
-
-        // file found at server
-        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
-        {
-          String payload = https.getString();
-          Serial.println(payload);
-        }
+        const String &payload = http.getString();
+        Serial.println("received payload:\n<<");
+        Serial.println(payload);
+        Serial.println(">>");
       }
-      else
-      {
-        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
-      }
-
-      https.end();
     }
     else
     {
-      Serial.printf("[HTTPS] Unable to connect\n");
+      Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
     }
+
+    http.end();
+  }
+  else
+  {
+    Serial.printf("Wifi Not Connected !\n");
   }
 
-  Serial.println("Wait 10s before next round...");
-  delay(10000);
+  // DynamicJsonDocument doc(1024);
+  // deserializeJson(doc, F("{\"sensor\":\"gps\",\"time\":1351824120,"
+  //                        "\"data\":[48.756080,2.302038]}"));
+  // JsonObject obj = doc.as<JsonObject>();
+  // long time = obj[F("time")];
+  // Serial.print("Json example ");
+  // Serial.print(time);
+  // Serial.println();
+  delay(3000);
 }
